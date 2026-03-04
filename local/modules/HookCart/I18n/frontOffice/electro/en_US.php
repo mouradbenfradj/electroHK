226dbcc55e1aca971ece9c4b3ecb16c0fe09c2b8
Mourad Ben Fradj <mourad.ben.fradj@gmail.com>
2026-03-04 22:58:27 +0100

    to deploy

---
+++ local/modules/HookCart/I18n/frontOffice/electro/en_US.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'Cart' => 'Cart',
+    'Checkout' => 'Checkout',
+    'Continue Shopping' => 'Continue Shopping',
+    'FREE delivery' => 'FREE delivery',
+    'Location in' => 'Location in',
+    'Remove' => 'Remove',
+    'Shop Cart' => 'Shop Cart',
+    'Update Cart' => 'Update Cart',
+    'View Cart' => 'View Cart',
+    'You have no items in your shopping cart.' => 'You have no items in your shopping cart.',
+    'checkout now!' => 'checkout now!',
+);
